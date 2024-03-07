2477d782c56f961b76a49b5e857168494992a9b0
alecascencio <alejandro.dma@outlook.com>
2024-03-07 17:51:21 -0600

    Gallery

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,34 @@
-'use client'
+'use client';
 
-import { useState } from "react";
-import Image from "next/image";
-import styles from "./page.module.css";
-import Navbar from "../components/Navbar/index.js"
-import Sidebar from "../components/Sidebar"
-import HeroSection from "../components/HeroSection"
-import InfoSection from "../components/InfoSection"
-import { Menu,Reservaciones,Nosotros,Historia,EquipoHumano,Contacto,Ubicacion } from "../components/InfoSection/Data"
-import Footer from "../components/Footer"
-import { whatsapp } from "../components/InfoSection/Data"
-import CardSlider from "../components/CardSlider"
-import  {
-  Button,
-} from 'react-bootstrap'
+import { useState } from 'react';
+import Image from 'next/image';
+import styles from './page.module.css';
+import Navbar from '../components/Navbar/index.js';
+import Sidebar from '../components/Sidebar';
+import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+import {
+  Menu,
+  Reservaciones,
+  Nosotros,
+  Historia,
+  EquipoHumano,
+  Contacto,
+  Ubicacion,
+} from '../components/InfoSection/Data';
+import Footer from '../components/Footer';
+import { whatsapp } from '../components/InfoSection/Data';
+import Gallery from '@/components/Gallery/Gallery';
+import CardSlider from '../components/CardSlider';
+import { Button } from 'react-bootstrap';
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState("closed");
+  const [isOpen, setIsOpen] = useState('closed');
 
   const toggle = () => {
-    console.log('toggle')
-    if (isOpen !== "closed") setIsOpen("closed");
-    else setIsOpen("open");
+    console.log('toggle');
+    if (isOpen !== 'closed') setIsOpen('closed');
+    else setIsOpen('open');
   };
 
   return (
@@ -32,10 +39,17 @@ export default function Home() {
       <InfoSection {...Nosotros} />
       <CardSlider />
       <InfoSection {...Reservaciones} />
+      <Gallery />
       {/* <InfoSection {...Ubicacion} /> */}
       <InfoSection {...Contacto} />
       <Footer />
-      <Button className='fixed-button-l' variant='success' onClick={()=>whatsapp()}><i className="bi bi-whatsapp"></i></Button> 
+      <Button
+        className='fixed-button-l'
+        variant='success'
+        onClick={() => whatsapp()}
+      >
+        <i className='bi bi-whatsapp'></i>
+      </Button>
     </>
   );
 }
---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './Gallery.css';
+
+function Gallery() {
+  return (
+    <section className='gallery container'>
+      <div>
+        <div className='wrapper'>
+          <div className='media'>
+            <div className='layer-1'>
+              <h1>SIN MIEDO</h1>
+              <p>¡Mejor ambiente!</p>
+              <p className='Inria'>
+                Conoce la variedad de bebidas y mixología que tenemos
+                especialmente para ti.
+              </p>
+            </div>
+            <img
+              className='img-1'
+              src='https://images.pexels.com/photos/2078008/pexels-photo-2078008.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              alt=''
+            />
+          </div>
+          <div className='media'>
+            <div className='layer-2'>
+              <h1>EL MEJOR SABOR</h1>
+              <p>¡Prueba las especialidades!</p>
+              <p className='Inria'>
+                ¡Ufffff! Podrás encontrar todo tipo de platillos y bebidas.
+              </p>
+            </div>
+            <img
+              className='img-2'
+              src='https://images.pexels.com/photos/4958641/pexels-photo-4958641.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              alt=''
+            />
+          </div>
+          <div className='media'>
+            <div className='layer-1'>
+              <h1>LAS INSTALACIONES</h1>
+              <p>¡Para todos y con todos!</p>
+              <p className='Inria'> Te hacemos sentir como en casa.</p>
+            </div>
+            <img
+              className='img-1'
+              src='https://images.pexels.com/photos/16172402/pexels-photo-16172402/free-photo-of-alcohol-barra-bar-botellas.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              alt=''
+            />
+          </div>
+          <div className='media'>
+            <div className='layer-2'>
+              <h1>DISFRUTA LOS EVENTOS</h1>
+              <p>¡Vive la pasión del deporte!</p>
+              <p className='Inria'>
+                Disfruta de los mejores partidos y eventos en compañía de tus
+                amigos y familia.
+              </p>
+            </div>
+            <img
+              className='img-2'
+              src='https://pbs.twimg.com/media/FZBHzLLWYAMlwgW?format=jpg&name=large'
+              alt=''
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Gallery;
